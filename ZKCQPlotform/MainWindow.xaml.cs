f7d4118648b7949ea2834e4843e9784cddf5a29e
c.lv <airlcade@163.com>
2019-09-23 00:23:54 +0800

    ui更新

--- ZKCQPlotform/MainWindow.xaml.cs
+++ ZKCQPlotform/MainWindow.xaml.cs
@@ -24,5 +24,59 @@ namespace ZKCQPlotform
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            bytecnt.Text = Properties.Settings.Default.usbbytecnt;
+            framecnt.Text = Properties.Settings.Default.usbframecnt;
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.usbbytecnt = bytecnt.Text;
+            Properties.Settings.Default.usbframecnt = framecnt.Text;
+
+            Properties.Settings.Default.Save();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void Searchdevice_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Usbconnect_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Usbsavedata_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Usbfilepath_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Netconnect_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Netsavedata_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Netchangepath_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
